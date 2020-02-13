177ff327c061aa31c9350ead0ce61f4077e8f457
jinwoo <wlsdn110@gmail.com>
2020-02-14 02:10:45 +0900

    collection_date
    올린날을 기준으로 collection 이름 지정

--- models/post.js
+++ models/post.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
+
+function getCurrentDate(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    var today = date.getDate();
+    collection_name=year.toString()+month.toString()+today.toString();
+    return collection_name;
+}
+
 const postSchema = new mongoose.Schema({
     title:{type: String, required:true},
     writer:{type: String, required:true},
@@ -8,6 +18,10 @@ const postSchema = new mongoose.Schema({
     reply_num:{type: Number, required:true, default:0},
     likes_num:{type: Number, required:true, default:0},
     createdAt:{type: Date, default:Date.now},
+},
+{
+    collection:getCurrentDate(),
+    timestamp:true
 });
 
-module.exports = mongoose.model('post', postSchema);
+module.exports = mongoose.model('Post', postSchema);
